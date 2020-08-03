e9eb177e75ed3e56e5277732738702fb9a626950
hanwenming <17600462006@163.com>
2020-08-03 22:00:09 +0800

    增加日志模块

--- control/login.go
+++ control/login.go
@@ -1,17 +1,19 @@
 package control
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	nowLog "org.beijing.com/Scaffolding-web-han/Scaffolding-web-han/log"
 )
 
 //登录页面访问
 func Login(c *gin.Context) {
 	c.HTML(http.StatusOK, "login.html", gin.H{})
-	//flog ,err := nowLog.Log_init()
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//	return
-	//}
-	//flog.Info("访问登录页面成功")
+	flog, err := nowLog.Log_init()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	flog.Info("访问登录页面成功")
 }
